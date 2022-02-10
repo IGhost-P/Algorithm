0a5b991b3f83e0f36f3a20965b8618e88dcac3be
IGhos-P <dndb3599@gmail.com>
2022-02-10 17:41:25 +0900

    왜 틀린지 모름

--- JS/백준 알고리즘/10930.js
+++ JS/백준 알고리즘/10930.js
@@ -1,3 +1,11 @@
 // SHA-256
 test_case = require("fs").readFileSync("예제.txt").toString();
-console.log(test_case);
+var crypto = require("crypto");
+var shasum = crypto.createHash("sha256");
+shasum.update(test_case);
+var output = shasum.digest("hex");
+
+// 한 번에 쓰는법
+//var output = crypto.createHash('sha512').update(id).shasum.digest('hex');
+
+console.log(output);
