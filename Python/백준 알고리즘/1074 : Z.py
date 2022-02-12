51cfdd8cf0bc80d7b5a12d4ad6d91934224456e8
IGhos-P <dndb3599@gmail.com>
2022-02-12 16:00:14 +0900

    정답

--- Python/백준 알고리즘/1074 : Z.py
+++ Python/백준 알고리즘/1074 : Z.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def recursive(n, x, y):
     global result
     if (n == 2):
@@ -25,5 +28,29 @@ def recursive(n, x, y):
 
 
 result = 0
-N, X, Y = map(int, input().split(' '))
+N, X, Y = map(int, sys.stdin.readline().split(' '))
 recursive(2**N, 0, 0)
+
+# import sys
+
+
+# def recursive(n, x, y):
+#     global result
+#     if x == X and y == Y:
+#         print(result)
+#         exit(0)
+#     if n == 1:
+#         result += 1
+#         return
+#     if not (x <= X < x + n and y <= Y < y + n):
+#         result += n * n
+#         return
+#     recursive(n/2, x, y)
+#     recursive(n/2, x, y + n/2)
+#     recursive(n/2, x + n/2, y)
+#     recursive(n/2, x + n/2, y + n/2)
+
+
+# result = 0
+# N, X, Y = map(int, sys.stdin.readline().split(' '))
+# recursive(2**N, 0, 0)
