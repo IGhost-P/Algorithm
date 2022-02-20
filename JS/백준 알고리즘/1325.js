fe5555c12cd9f01d2607328af9ae79ddd7ce453b
IGhos-P <dndb3599@gmail.com>
2022-02-20 16:22:53 +0900

    정답

--- JS/백준 알고리즘/1325.js
+++ JS/백준 알고리즘/1325.js
@@ -9,49 +9,38 @@
 input = require("fs").readFileSync("예제.txt").toString().trim().split("\n");
 [N, M] = input.shift().split(" ").map(Number);
 tree = Array.from(Array(N + 1), () => Array().fill([]));
-let answerList = [];
 for (let i = 0; i < M; i++) {
-  [from, to] = input[i].split(" ");
+  [from, to] = input[i].split(" ").map(Number);
   tree[to].push(from);
 }
 
-let cnt;
-let cntMax = [];
-
-const BFS = function (visited, startNode) {
-  visited[startNode] = true;
-  q = [];
-  q.push(startNode);
-  cnt = 1;
-  while (q.length > 0) {
-    curNode = q.shift();
-    tree[curNode].forEach((el) => {
-      if (!visited[el]) {
-        q.push(el);
-        visited[el] = true;
-        cnt++;
-      }
-    });
-  }
-};
+let count = Array(N + 1).fill(1);
 let visited = new Array(N + 1).fill(false);
-let maxValue = -1;
+let result = [];
 
-const solutuion = () => {
-  for (let i = 1; i <= N; i++) {
-    startNode = i;
-    visited.fill(false);
-    if (!visited[startNode]) {
-      BFS(visited, startNode);
-      //   console.log(visited, startNode, cnt);
-      if (cnt > maxValue) {
-        answerList.push(startNode);
-        maxValue = cnt;
-      } else if (cnt == maxValue) {
-        answerList.push(startNode);
-      }
+const DFS = function (startNode, computer) {
+  visited[computer] = true;
+  if (tree[computer].length === 0) {
+    return;
+  }
+  for (let i = 0; i < tree[computer].length; i++) {
+    const nextNode = tree[computer][i];
+    if (!visited[nextNode]) {
+      count[startNode]++;
+      DFS(startNode, nextNode);
     }
   }
 };
+let max;
+const solutuion = () => {
+  for (let i = 1; i <= N; i++) {
+    DFS(i, i);
+    visited.fill(false);
+  }
+  max = Math.max(...count);
+  for (let i = 1; i < count.length; i++) {
+    if (count[i] === max) result.push(i);
+  }
+  console.log(result.join(" "));
+};
 solutuion();
-console.log(answerList.join(" ") || 0);
