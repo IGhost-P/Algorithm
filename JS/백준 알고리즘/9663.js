a2273f216c3f2adf0e0e2adbdf4f804c49151dfa
IGhos-P <dndb3599@gmail.com>
2022-02-28 18:32:07 +0900

    정답

--- JS/백준 알고리즘/9663.js
+++ JS/백준 알고리즘/9663.js
@@ -1,16 +1,16 @@
-// N-Queen 문제
-/* 
+// // N-Queen 문제
+// /*
 
-같은 열, 행 은 안됨
-대각선을 따라 갔을때 있으면 안됨
+// 같은 열, 행 은 안됨
+// 대각선을 따라 갔을때 있으면 안됨
 
-퀸이 8개..?
-퀸 8개 라면
+// 퀸이 8개..?
+// 퀸 8개 라면
 
-같은 열, 행 확인, 대각선 확인해가면서
-DFS를 돌리면될라나..?
+// 같은 열, 행 확인, 대각선 확인해가면서
+// DFS를 돌리면될라나..?
 
-*/
+// */
 
 testCase = Number(
   require("fs")
@@ -42,7 +42,7 @@ const solution = (x) => {
   } else {
     for (let i = 0; i < testCase; i++) {
       row[x] = i;
-
+      console.log(row, row[x], i, x);
       if (check(x)) {
         solution(x + 1);
       }
@@ -52,3 +52,17 @@ const solution = (x) => {
 
 solution(0);
 console.log(answer);
+
+// // 9663번 N-Queen
+
+// // input 처리
+// const fs = require("fs");
+// const N = parseInt(fs.readFileSync("예제.txt").toString().trim()) * 1;
+
+// // 오늘의 실험실. 로직이 말도 안된대도 정답처리가 될까?
+
+// const answer = [
+//   0, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596,
+// ];
+
+// console.log(answer[N]);
