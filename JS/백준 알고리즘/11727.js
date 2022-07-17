1c4fbe7d7432b9f68dde8e87026b50b42095462d
IGhos-P <dndb3599@gmail.com>
2022-07-18 01:38:19 +0900

    resolved : 11727

--- JS/백준 알고리즘/11727.js
+++ JS/백준 알고리즘/11727.js
@@ -1,30 +1,14 @@
 // 11727 : 2*n 타일링 2
-/**
- * 1*2 , 2*1 , 2*2 타일
- * 2*1 = 2*1 로 1개
- * 2*2 = 1*2로 2개 , 2*1로 2개 2*2 로 1개 = 3개
- * 2*3 = 2*2에다가 2*1로   를 양쪽에 끼움 = *2 + 1(양쪽에 끼움)
- * 2*4 = 2*2의 방법의 제곱
- * 2*5 = 2*4의 방법 그대로
- *
- * 점화식으로 표현해보자면 , 1,3,3,6.. 이니, fn이 짝수 이면 -2의 2배
- * fn이 홀수 이면 -1이 그대로
- */
 
 testCase = require("fs")
   .readFileSync(process.platform === "linux" ? "dev/stdin" : "예제.txt")
   .toString()
   .trim();
 
-const arr = [1, 3, 6, 9];
-for (let i = 4; i <= testCase; i++) {
-  if (i % 2 != 0) {
-    arr[i] = arr[i - 2] * 2 + arr[i - 2];
-  } else {
-    arr[i] = arr[i - 1] * 2;
-  }
+const arr = [1, 3];
+for (let i = 2; i <= testCase; i++) {
+  arr[i] = ((arr[i - 1] % 10007) + ((arr[i - 2] * 2) % 10007)) % 10007;
 }
 
-console.log(arr);
-
-console.log(arr[testCase]);
+console.log(arr)
+console.log(arr[testCase - 1]);
