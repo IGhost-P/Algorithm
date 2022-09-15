c7b783a6e8b123b2ed2a3130a8e1b0a23f274680
sangwooyang <dndb3599@gmail.com>
2022-09-15 20:49:00 +0900

    resolved : ox퀴즈

--- JS/백준 알고리즘/practice.js
+++ JS/백준 알고리즘/practice.js
@@ -1,30 +1,21 @@
-const bfs = (row, col) => {
-  let queue = [[row, col]];
-  let count = 0;
-  let visited = Array.from(Array(rowLen), () => Array(colLen).fill(false));
-  visited[row][col] = true;
+[N, ...testCase] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
 
-  while (queue.length > 0) {
-    let [r, c] = queue.pop();
-    if (r === rowLen - 1 && c === colLen - 1) return count;
-
-    const dr = [-1, 1, 0, 0];
-    const dc = [0, 0, -1, 1];
-
-    for (let i = 0; i < 4; i++) {
-      let nr = r + dr[i];
-      let nc = c + dc[i];
-      if (nr < 0 || nr >= rowLen || nc < 0 || nc >= colLen) continue;
-      if (visited[nr][nc]) continue;
-      if (map[nr][nc] === 0) continue;
-      visited[nr][nc] = true;
-      queue.push([nr, nc]);
-    }
-    count++;
+const solution = (N, arr) => {
+  let answer = [];
+  for (let i = 0; i < N; i++) {
+    answer.push(
+      arr[i].split("X").reduce((acc, cur) => {
+        const len = cur.length;
+        return acc + (len * (len + 1)) / 2;
+      }, 0)
+    );
   }
-};
-function solution(maps) {
-  dfs(0, 0);
 
-  return "";
-}
+  return answer.join("\n");
+};
+
+console.log(solution(N, arr));
