7755d750ed3eeb45c0a837674a2b8d13ba343628
sangwooyang <dndb3599@gmail.com>
2022-09-16 14:42:18 +0900

    resovled : 네트워크

---
+++ JS/프로그래머스/고득점KIT/네트워크/index.js
@@ -0,0 +1,41 @@
+const setGraph = (n, computers) => {
+  const graph = new Map();
+  computers.forEach((computer, index) => {
+    computer.forEach((isConnected, i) => {
+      if (isConnected === 1) {
+        if (graph.has(index)) {
+          graph.set(index, [...graph.get(index), i]);
+        } else {
+          graph.set(index, [i]);
+        }
+      }
+    });
+  });
+  return graph;
+};
+
+function solution(n, computers) {
+  var answer = 0;
+  const graph = setGraph(n, computers);
+  while (graph.size > 0) {
+    const start = graph.keys().next().value;
+    const stack = [start];
+    while (stack.length > 0) {
+      const node = stack.pop();
+      if (graph.has(node)) {
+        stack.push(...graph.get(node));
+        graph.delete(node);
+      }
+    }
+    answer++;
+  }
+  return answer;
+}
+
+console.log(
+  solution(3, [
+    [1, 1, 0],
+    [1, 1, 0],
+    [0, 0, 1],
+  ])
+);
