4de5cdb553615cf47430f57a154d71b34ba685a4
sangwooyang <dndb3599@gmail.com>
2022-11-01 00:23:20 +0900

    docs : 이름 변경

--- JS/practice.js
+++ JS/practice.js
@@ -1,18 +1,71 @@
-function createReactiveObject(target, callback) {
-  const proxy = new Proxy(target, {
-    set(obj, prop, value) {
-      if (value != obj[prop]) {
-        const prev = obj[prop];
-        obj[prop] = value;
-        callback(`Property ${prop} changed from ${prev} to ${value}`);
+class Node {
+  constructor(value = "") {
+    this.value = value;
+    this.children = new Map();
+  }
+}
+class Trie {
+  constructor() {
+    this.root = new Node();
+  }
+
+  has(string) {
+    let currNode = this.root;
+
+    for (const char of string) {
+      if (!currNode.children.has(char)) {
+        return false;
       }
-      return true;
-    },
-  });
-  return proxy;
+      currNode = currNode.children.get(char);
+    }
+    return currNode;
+  }
+
+  insert(string) {
+    let currNode = this.root;
+    const word = string;
+
+    for (const char of word) {
+      if (!currNode.children.has(char)) {
+        currNode.children.set(char, new Node(currNode.value + char));
+      }
+      currNode = currNode.children.get(char);
+    }
+  }
+
+  autoComplete(string) {
+    const currNode = this.has(string);
+    if (!currNode) return [];
+
+    const words = [];
+    const queue = [currNode];
+
+    while (queue.length) {
+      const currNode = queue.shift();
+
+      if (currNode.children.size === 0) {
+        words.push(currNode.value);
+      }
+
+      for (const child of currNode.children.values()) {
+        queue.push(child);
+      }
+    }
+
+    return words;
+  }
 }
 
-const a = { name: "John", age: 25 };
-const b = createReactiveObject(a, (message) => console.log(message));
-b.name = "Jane2";
-b.age = 26;
+// 트라이 생성
+const trie = new Trie();
+trie.insert("cat");
+trie.insert("cats");
+trie.insert("can");
+trie.insert("candy");
+
+// 자동 완성 테스트
+console.log("입력: ca, 결과: ", trie.autoComplete("ca")); //['cat', 'can', 'cats', 'candy']
+console.log("입력: cat, 결과: ", trie.autoComplete("cat")); //['cat', 'cats']
+console.log("입력: candy, 결과: ", trie.autoComplete("candy")); //['candy']
+console.log("입력: ct, 결과: ", trie.autoComplete("ct")); //null
+console.log("입력: dog, 결과: ", trie.autoComplete("dog")); //null
